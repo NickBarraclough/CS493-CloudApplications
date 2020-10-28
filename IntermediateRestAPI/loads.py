ad8542d3a3be2587ceff787d976f1be2acbd138d
NickBarraclough <46543465+NickBarraclough@users.noreply.github.com>
2020-10-28 16:15:56 -0700

    Update and rename loads.py to IntermediateRestAPI/loads.py

--- IntermediateRestAPI/loads.py
+++ IntermediateRestAPI/loads.py
@@ -1,4 +1,9 @@
-import datetime
+#---------------------------------------------------------------------
+# Assigment 4: Intermediate REST API
+#   \__loads.py
+# Datastore request handlers for /loads/*. 
+#---------------------------------------------------------------------
+
 import json
 import boats
 
@@ -9,38 +14,29 @@ client = datastore.Client()
 
 bp = Blueprint('loads', __name__, url_prefix='/loads')
 
+# LOADS request handler - GET (All loads) and POST (Add new load)
 @bp.route('', methods=['GET', 'POST'])
 def loads_get_post():
     if request.method == 'POST':
+        
         content = request.get_json() # Retreive content from request body
+        
+        # Build new load and assign request attributes
         if len(content) == 4: # Correct amount of attributes
-            # Build new load and assign attributes
-            new_load = datastore.entity.Entity(key=client.key("loads"))
-            new_load.update({"weight": content["weight"], "carrier": None,
-          "content": content["content"], "delivery_date": content["delivery_date"]})
-            client.put(new_load)
-            success = {"id": new_load.key.id, "weight": content["weight"], "content": content["content"],
-          "delivery_date": content["delivery_date"], "self": (request.url + '/' + str(new_load.key.id)), "carrier": None}
+            new_load = datastore.entity.Entity(key=client.key("loads")) # Grab a loads entity from Datastore
+            new_load.update({"weight": content["weight"], "carrier": None, "content": content["content"], "delivery_date": content["delivery_date"]})
+            client.put(new_load) # Update Datastore entity
+            success = {"id": new_load.key.id, "weight": content["weight"], "content": content["content"], "delivery_date": content["delivery_date"], "self": (request.url + '/' + str(new_load.key.id)), "carrier": None}
             return (success, 201)
         else: # Incorrect amount of attributes
             failure = {"Error": "The request object is missing at least one of the required attributes"}
             return (failure, 400)
         
-#    if request.method == 'GET': # Get all loads
-#        query = client.query(kind="loads") # Query datastore for all loads
-#        results = list(query.fetch())
-#        for e in results:
-#            e["id"] = e.key.id
-#            e["self"] = request.url + '/' + str(e.key.id)
-#        return (jsonify(results), 200)
-#
-#    else:
-#        failure = {"Error": "Could not get loads"}
-#        return (failure, 404)
+    # Paginated list of all loads (Max 3)
     elif request.method == 'GET':
-        query = client.query(kind="loads")
-        q_limit = int(request.args.get('limit', '3'))
-        q_offset = int(request.args.get('offset', '0'))
+        query = client.query(kind="loads") # Query datastore for all loads
+        q_limit = int(request.args.get('limit', '3')) # Query has a list limit of 3
+        q_offset = int(request.args.get('offset', '0')) # Set offset
         g_iterator = query.fetch(limit= q_limit, offset=q_offset)
         pages = g_iterator.pages
         results = list(next(pages))
@@ -62,33 +58,44 @@ def loads_get_post():
 # LOADS request handler - GET(Specified)
 @bp.route('/<load_id>', methods=['GET', 'DELETE'])
 def loads_id_get_delete(load_id):
+    # GET load
     if request.method == 'GET':
+        
         load_key = client.key("loads", int(load_id))
         load = client.get(key=load_key)
-        if load == None: # Load not found
+        
+        # Load not found
+        if load == None:
             failure = {"Error": "No load with this load_id exists"}
             return (failure, 404)
+        
         # Load found, set id and self
         load["id"] = load_id
         load["self"] = request.url
         return (load, 200)
 
+    # DELETE load
     elif request.method == 'DELETE':
+        
         load_key = client.key("loads", int(load_id))
         load = client.get(key=load_key)
+        
+        # Load not found
         if load == None:
             failure = {"Error": "No load with this load_id exists"}
             return (failure, 404)
 
+        # Carrier boat must remove this load before it is deleted
         if load["carrier"] != None:
             boat_key = client.key("boats", int(load["carrier"]["id"]))
             boat = client.get(key=boat_key)
-            for _ in boat["loads"]:
-                if _["id"] == load.key.id:
-                    boat["loads"].remove({"id": load.key.id})
-            client.put(boat)
+            for _ in boat["loads"]: # Search each load on boat
+                if _["id"] == load.key.id: # Find matching load
+                    boat["loads"].remove({"id": load.key.id}) # Remove from boat's loads list
+            client.put(boat) # Update boat in Datastore
         
         client.delete(load_key)
+        
         return('', 204)
     
     else:
